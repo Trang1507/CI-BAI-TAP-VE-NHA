bc8b6ff53912644691b845ca6aff1d51e267f2fb
Trang1507 <thuytrang150715@gmail.com>
2021-12-06 23:22:42 +0700

    update

--- DAY2/bai2.js
+++ DAY2/bai2.js
@@ -8,20 +8,13 @@ class question_and_Answer {
     $question3;
     $answer3;
 
-    answer1_appear = () => {
-        this.$answer1.style.display = 'block';
-        this.$question1.addEventListener('click',this.answer1_disappear)    
-        this.$question1.addEventListener('click',this.answer1_appear)
-    }
 
-    answer1_disappear = ()=>{
-        this.$answer1.style.display = 'none'
-        this.$question1.addEventListener('click',this.answer1_appear)
-    }
 
     constructor(){
         this.$container = document.createElement('div')
         this.$container.classList.add('container')
+        this.$container.innerHTML = "Question and Answer"
+
 
         this.$question1=document.createElement('div')
         this.$question1.classList.add('question1')
@@ -33,12 +26,66 @@ class question_and_Answer {
         this.$answer1.innerHTML="Clothing & Bag"
         this.$answer1.style.display = 'none'
     
+        this.$question2=document.createElement('div')
+        this.$question2.classList.add('question2')
+        this.$question2.innerHTML='What kind of payment you are offering?'
+        this.$question2.addEventListener('click', this.answer2_appear);
+
+        this.$answer2=document.createElement('div')
+        this.$answer2.classList.add('answer2')
+        this.$answer2.innerHTML="POS and cash"
+        this.$answer2.style.display = 'none'
+
+
+        this.$question3=document.createElement('div')
+        this.$question3.classList.add('question2')
+        this.$question3.innerHTML='Do you offer a refund?'
+        this.$question3.addEventListener('click', this.answer3_appear);
+
+        this.$answer3=document.createElement('div')
+        this.$answer3.classList.add('answer3')
+        this.$answer3.innerHTML="Yes"
+        this.$answer3.style.display = 'none'
+
+    }
+
+    answer1_appear = () => {
+
+        if (this.$answer1.style.display == 'block'){
+            this.$answer1.style.display = 'none'
+        }
+        else {
+            this.$answer1.style.display = 'block'
+        }
+    }
+
+    answer2_appear = () => {
+
+        if (this.$answer2.style.display == 'block'){
+            this.$answer2.style.display = 'none'
+        }
+        else {
+            this.$answer2.style.display = 'block'
+        }
+    }
+
+    answer3_appear = () => {
+
+        if (this.$answer3.style.display == 'block'){
+            this.$answer3.style.display = 'none'
+        }
+        else {
+            this.$answer3.style.display = 'block'
+        }
     }
 
     render(){
         this.$container.appendChild(this.$question1)
-        
-        this.$container.appendChild(this.$answer1) 
+        this.$question1.appendChild(this.$answer1) 
+        this.$container.appendChild(this.$question2)
+        this.$question2.appendChild(this.$answer2) 
+        this.$container.appendChild(this.$question3)
+        this.$question3.appendChild(this.$answer3) 
         return this.$container
     }
 
